84613a24894b8813296b1210594bd7e8cac06c5c
Marek Vybíral <me@marekvybiral.com>
2021-01-14 21:09:14 +0100

    Modified        1 file(s), Added        0 file(s), Removed        0 file(s)
    M  src/scheduler.py

--- src/scheduler.py
+++ src/scheduler.py
@@ -7,6 +7,7 @@ from backup import do_backup
 for schedule_entry in os.environ['SCHEDULE'].split(','):
     schedule_entry = schedule_entry.strip()
     schedule.every().day.at(schedule_entry).do(do_backup)
+    
     print(f'Scheduling for {schedule_entry}')
 
 print('\nScheduler is running...')
